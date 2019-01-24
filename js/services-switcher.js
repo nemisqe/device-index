9b5c6ee2159a4cebc96a5b8a2f1bc9c9d421f39c
nemisqe <41796565+nemisqe@users.noreply.github.com>
2019-01-24 22:49:33 +0300

    service-block tab switching added

--- js/services-switcher.js
+++ js/services-switcher.js
@@ -1,6 +1,50 @@
 'use strict';
 
-// клик на тоггл - добавление класса "button-active" и удаление этого класса у всех тоглов кроме текущего.
+// клик на таб => добавление класса "button-active" и удаление этого класса у всех табов кроме текущего + добавление класса "_active" у соответствующего таб-контента
 
+var tabs = document.querySelectorAll('.info-toggle');
+var tabsContents = document.querySelectorAll('.service-info__description--container');
 
+for (let i = 0; i < tabs.length; i++) {
+	let tab = tabs[i];
 
+	onTabClick(tab, i);
+}
+
+function onTabClick (tab, i) {
+	tab.addEventListener('click', function () {
+
+	toggleNextActiveTabContent(i);
+	});
+};
+
+///////////IMP/////////////
+
+function toggleNextActiveTabContent (i) {
+	disableActiveTabContent();
+	disableActiveTab();
+	enableTabContent(i);
+	enableTab(i);
+};
+
+function disableActiveTabContent () {
+	var activeTabContent = document.querySelector('._active');
+
+	activeTabContent.classList.remove('_active');
+};
+
+function disableActiveTab () {
+	var activeTab = document.querySelector('.button-active');
+
+	activeTab.classList.remove('button-active');
+};
+
+function enableTabContent (i) {
+	var nextActiveTabContent = tabsContents[i];
+	nextActiveTabContent.classList.add('_active');
+};
+
+function enableTab (i) {
+	var nextActiveTab = tabs[i];
+	nextActiveTab.classList.add('button-active');
+};
---
+++ js/slider-switcher.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var toggles = document.querySelectorAll('.slider-toggle');
+
+var slideBindings = {
+	slide1: {
+		img: document.querySelector('.slider-one-pic'),
+		title: document.querySelector('.slider-title'),
+		number: document.querySelector('.header-number'),
+		description: document.querySelector('.hit-product__description'),
+		table: document.querySelector('.hit-product-info__data')
+	},
+
+	slide2: {
+		img: document.querySelector('.slider-one-pic'),
+		title: document.querySelector('.slider-title'),
+		number: document.querySelector('.header-number'),
+		description: document.querySelector('.hit-product__description'),
+		table: document.querySelector('.hit-product-info__data')
+	},
+
+	slide3: {
+		img: document.querySelector('.slider-one-pic'),
+		title: document.querySelector('.slider-title'),
+		number: document.querySelector('.header-number'),
+		description: document.querySelector('.hit-product__description'),
+		table: document.querySelector('.hit-product-info__data')
+	}
+};
